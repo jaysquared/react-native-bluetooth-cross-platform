1fd9fa6dcf254acc9ce7d6b695e7432ffed10da8
MikhailVasiliev <mr.m.vasiliev@gmail.com>
2020-04-15 21:09:03 +0300

    remove .characters

--- ios/react-native-bluetooth-cross-platform/ParseExtensions.swift
+++ ios/react-native-bluetooth-cross-platform/ParseExtensions.swift
@@ -2,13 +2,13 @@ import Foundation
 
 extension String {
   func getIndexOf(_ substring: String) -> Int? {
-    let subLength = substring.characters.count
-    let length = self.characters.count
+    let subLength = substring.count
+    let length = self.count
     if(subLength > length) {
       return nil;
     }
     for i in 0..<(length - subLength) {
-      let sample = self.substring(with: self.characters.index(self.startIndex, offsetBy: i)..<self.characters.index(self.startIndex, offsetBy: i + subLength))
+      let sample = self.substring(with: self.index(self.startIndex, offsetBy: i)..<self.index(self.startIndex, offsetBy: i + subLength))
       if substring == sample {
         return i
       }
